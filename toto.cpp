490f346c1458e93a159c05f788d0dec604f8ae75
Flore mkb <florneko@gmail.com>
2011-05-10 16:36:54 +0200

    Tracé de g(w) fonction chapeau Ajout de l''écart entre r simulé et r théorique et de son évolution avec le nombre d''oscillateurs

--- toto.cpp
+++ toto.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 	srand(time(NULL));
 
 	/*définition des oscillatteurs*/
-	int idxOsc, idxTime, idxK, idxRand;
+	int idxOsc, idxTime, idxK, idxRand, idxN;
 	
 	double rayontemp = 0, psitemp = 0;
 	
@@ -57,6 +57,7 @@ int main(int argc, char *argv[])
 	double K = 0;
 	double OMEGA = 0;
 	double sigma = 0.1;
+//	double subcrit = 0.2;
 	double *rayon = (double *) malloc (nbsamples*sizeof(double));
 	double *psi = (double *) malloc (nbsamples*sizeof(double));
 	double *rayonmoyen = (double *) malloc (nbsamples*sizeof(double));
@@ -65,6 +66,23 @@ int main(int argc, char *argv[])
 	double *rayonstable = (double *) malloc (nbK*sizeof(double));
 	double *invrayonstable = (double *) malloc (nbK*sizeof(double));
 	double *Kvect = (double *) malloc (nbK*sizeof(double));
+
+	double *ecartMax = (double *) malloc (nbosc*sizeof(double));
+
+	/*Définition pour les logarithmes*/
+	int idxKc = 0;
+	int idxprim = 0;
+	double *logk = (double *) malloc ((nbK-idxKc)*sizeof(double));
+	double *logr = (double *) malloc ((nbK-idxKc)*sizeof(double));
+	double *deltarstable = (double *) malloc ((nbK-idxKc)*sizeof(double));
+	double *KvectCut = (double *) malloc ((nbK-idxKc)*sizeof(double));
+	double *TcCut = (double *) malloc ((nbK-idxKc)*sizeof(double));
+		
+	/*Détermination de la distribution des pulsations propres*/
+	int nbw = 1001;
+	int idxw;
+	double *w = (double *) malloc (nbw*sizeof(double));
+	double *N = (double *) malloc (nbw*sizeof(double));
 
 	/*Détermination de Kc*/
 	double Kc;
@@ -84,172 +102,181 @@ int main(int argc, char *argv[])
 	double k1, k2, k3, k4;
 
 
-	/*Boucle sur les valeurs de K*/
-	for(idxK = 0 ; idxK < nbK ; idxK++) {
-		if (nbK == 1) {
-			printf("entrer la valeur de K\n");
-			scanf("%lf",&K);
-		}
-		else {
-			K = (double)idxK / (nbK - 1);
-		}
+	for(idxN = 0 ; idxN < nbosc ; idxN++) {
 
-
-		rayonstable[idxK] = 0;
-	       	int idxTimeStart;
-	        idxTimeStart = (int)floor(3 * nbsamples / 4.);
-
-		printf("K= %f\n",K);
-
-		for (idxTime=1 ; idxTime < nbsamples ; idxTime++) {
-			rmoy[idxTime] = 0;
-		}
-
-
-		/*Boucle sur les réalisations*/
-		for (idxRand = 0 ; idxRand < nbrand ; idxRand++) {
-			/*Détermination des oscillations propres des oscillateurs*/
-			const gsl_rng_type * randType;
-			gsl_rng * r;
-			gsl_rng_env_setup();
-			randType = gsl_rng_default;
-			r = gsl_rng_alloc (randType);
-
-			for(idxOsc = 0 ; idxOsc < nbosc ; idxOsc++) {
-//				omega[idxOsc] = OMEGA+gsl_ran_gaussian(r,sigma);
-				omega[idxOsc] = OMEGA+gsl_ran_cauchy(r,sigma);
+		/*Boucle sur les valeurs de K*/
+		for(idxK = 0 ; idxK < nbK ; idxK++) {
+			if (nbK == 1) {
+				printf("entrer la valeur de K\n");
+				scanf("%lf",&K);
+			}
+			else {
+				K = (double)idxK * 2 / (nbK - 1);
 			}
 
 
-			/*Initialisation de theta, de rayon et de psi*/
-			for(idxOsc = 0 ; idxOsc < nbosc ; idxOsc++) {
-				theta[idxOsc] = fmod( rand(), 2*M_PI)-M_PI;
+			rayonstable[idxK] = 0;
+	       		int idxTimeStart;
+	        	idxTimeStart = (int)floor(3 * nbsamples / 4.);
+	
+	
+			for (idxTime=1 ; idxTime < nbsamples ; idxTime++) {
+				rmoy[idxTime] = 0;
 			}
-
-			for(idxTime = 0 ; idxTime < nbsamples ; idxTime++) {
-				rayon[idxTime]=0;
-				psi[idxTime]=0;
-			}
-
-			meanField(theta , &rayontemp, &psitemp, nbosc);
-			rayon[0] = rayontemp;
-			psi[0] = psitemp;
-
-
-			/*Corps du programme*/
-
-			for (idxTime = 1 ; idxTime < nbsamples ; idxTime++) {
-		      	temps[idxTime] = idxTime*deltaT;
-
-				for (idxOsc = 0 ; idxOsc < nbosc ; idxOsc++) {
-					/*Méthode de runge-kutta d'ordre 4*/
-					k1 = deltaT * kuramoto(omega[idxOsc], K, psi[idxTime-1], rayon[idxTime-1], theta[idxOsc]);
-					k2 = deltaT * kuramoto(omega[idxOsc], K, psi[idxTime-1], rayon[idxTime-1], theta[idxOsc] + deltaT * k1 / 2.0);
-					k3 = deltaT * kuramoto(omega[idxOsc], K, psi[idxTime-1], rayon[idxTime-1], theta[idxOsc] + deltaT * k2 / 2.0);
-					k4 = deltaT * kuramoto(omega[idxOsc], K, psi[idxTime-1], rayon[idxTime-1], theta[idxOsc] + deltaT * k3);
-					theta[idxOsc] = theta[idxOsc] + (k1 + 2 * k2 + 2 * k3 + k4) * deltaT / 6.0;
+	
+	
+			/*Boucle sur les réalisations*/
+			for (idxRand = 0 ; idxRand < nbrand ; idxRand++) {
+				/*Détermination des oscillations propres des oscillateurs*/
+				const gsl_rng_type * randType;
+				gsl_rng * r;
+				gsl_rng_env_setup();
+				randType = gsl_rng_default;
+				r = gsl_rng_alloc (randType);
+		
+	
+				for(idxOsc = 0 ; idxOsc < nbosc ; idxOsc++) {
+//					omega[idxOsc] = OMEGA + gsl_ran_gaussian(r,sigma);
+					omega[idxOsc] = OMEGA + gsl_ran_cauchy(r,sigma);
+//					omega[idxOsc] = OMEGA + (gsl_ran_cauchy(r, sigma) - subcrit + gsl_ran_cauchy(r, sigma) + subcrit) / 2;
 				}
-
+	
+	
+				/*Initialisation de theta, de rayon et de psi*/
+				for(idxOsc = 0 ; idxOsc < nbosc ; idxOsc++) {
+					theta[idxOsc] = fmod( rand(), 2*M_PI)-M_PI;
+				}
+	
+				for(idxTime = 0 ; idxTime < nbsamples ; idxTime++) {
+					rayon[idxTime]=0;
+					psi[idxTime]=0;
+				}
+	
 				meanField(theta , &rayontemp, &psitemp, nbosc);
-   			   	rayon[idxTime]=rayontemp;
-       			psi[idxTime]=fmod(psitemp, 2 * M_PI);
-
-				if (idxTime > idxTimeStart)
-					rayonstable[idxK] += rayontemp;
-
-				rmoy[idxTime]+=rayontemp;
+				rayon[0] = rayontemp;
+				psi[0] = psitemp;
+	
+	
+				/*Corps du programme*/
+	
+				for (idxTime = 1 ; idxTime < nbsamples ; idxTime++) {
+			      	temps[idxTime] = idxTime*deltaT;
+	
+					for (idxOsc = 0 ; idxOsc < nbosc ; idxOsc++) {
+						/*Méthode de runge-kutta d'ordre 4*/
+						k1 = deltaT * kuramoto(omega[idxOsc], K, psi[idxTime-1], rayon[idxTime-1], theta[idxOsc]);
+						k2 = deltaT * kuramoto(omega[idxOsc], K, psi[idxTime-1], rayon[idxTime-1], theta[idxOsc] + deltaT * k1 / 2.0);
+						k3 = deltaT * kuramoto(omega[idxOsc], K, psi[idxTime-1], rayon[idxTime-1], theta[idxOsc] + deltaT * k2 / 2.0);
+						k4 = deltaT * kuramoto(omega[idxOsc], K, psi[idxTime-1], rayon[idxTime-1], theta[idxOsc] + deltaT * k3);
+						theta[idxOsc] = theta[idxOsc] + (k1 + 2 * k2 + 2 * k3 + k4) * deltaT / 6.0;
+					}
+	
+					meanField(theta , &rayontemp, &psitemp, nbosc);
+   				   	rayon[idxTime]=rayontemp;
+       				psi[idxTime]=fmod(psitemp, 2 * M_PI);
+	
+					if (idxTime > idxTimeStart)
+						rayonstable[idxK] += rayontemp;
+	
+					rmoy[idxTime]+=rayontemp;
+				}
+	
+	
 			}
+	
+			/*Fin de la boucle sur les réalisations*/
 
-
-		}
-
-		/*Fin de la boucle sur les réalisations*/
-
-		/*Détermination de rmoy, moyenne de r sur plusieurs réalisations*/
-		for (idxTime = 1 ; idxTime < nbsamples ; idxTime++) {
-			rmoy[idxTime]/=nbrand;
-		}
-
-		/*Détermination de rayonstable et de son inverse*/
-		rayonstable[idxK] /= (nbsamples - idxTimeStart + 1) * nbrand;
-		invrayonstable[idxK] = 1 / (1 - rayonstable[idxK] * rayonstable[idxK]);//pour formule 4.7
-
-		/*Détermination du vecteur Kvect*/
-		Kvect[idxK] = K;
-
-
-		/*Définition de rMax*/
-		for (idxTime = 1 ; idxTime < nbsamples ; idxTime++) {
-			if (rmoy[idxTime] > rMax)
-				rMax = rmoy[idxTime];
-		}
-
-
-		/*Définition du temps critique comme étant le temps pour lequel on atteint 90% de la valeur maximale*/
-		idxTime = 0;
-		while (rmoy[idxTime] < 0.9 * rMax) {
-			idxTime++;
-		}
-		IdxC = idxTime;
-		Tc[idxK] = IdxC*deltaT;
-
-		/*Détermination de rayonInfini*/
-		if (IdxC < nbsamples) {
-			for (idxTime = IdxC; idxTime<nbsamples; idxTime++) {
-				rayonCut[idxTime-IdxC] = rmoy[idxTime];
-			}
-			rayonInfini[idxK] = gsl_stats_mean(rayonCut, 1, nbsamples-IdxC+1);
-//			printf("%g pour K = %d \n",rayonInfini[idxK], idxK);
-		}
-		else {
-//			printf("%g pour K= %d \n", rayonInfini[idxK], idxK);
-			rayonInfini[idxK] = gsl_stats_mean(rmoy, 1, nbsamples);
-/*car sinon on obtient des valeurs nulles du rayonInfini pour certaines valeurs de K<Kc*/
-		}
-
-
-		if (nbK == 1) {
-			rayonmoyen[0] = rayon[0];
+			/*Détermination de rmoy, moyenne de r sur plusieurs réalisations*/
 			for (idxTime = 1 ; idxTime < nbsamples ; idxTime++) {
-				rayonmoyen[idxTime] = rayonmoyen[idxTime-1] + rayon[idxTime];
+				rmoy[idxTime]/=nbrand;
 			}
-			for (idxTime = 0 ; idxTime < nbsamples ; idxTime++) {
-				rayonmoyen[idxTime] /= idxTime+1;
+	
+			/*Détermination de rayonstable et de son inverse*/
+			rayonstable[idxK] /= (nbsamples - idxTimeStart + 1) * nbrand;
+			invrayonstable[idxK] = 1 / (1 - rayonstable[idxK] * rayonstable[idxK]);//pour formule 4.7
+	
+			/*Détermination du vecteur Kvect*/
+			Kvect[idxK] = K;
+
+	
+			/*Définition de rMax*/
+			for (idxTime = 1 ; idxTime < nbsamples ; idxTime++) {
+				if (rmoy[idxTime] > rMax)
+					rMax = rmoy[idxTime];
 			}
+
+
+			/*Définition du temps critique comme étant le temps pour lequel on atteint 90% de la valeur maximale*/
+			idxTime = 0;
+			while (rmoy[idxTime] < 0.9 * rMax) {
+				idxTime++;
+			}
+			IdxC = idxTime;
+			Tc[idxK] = IdxC*deltaT;
+
+			/*Détermination de rayonInfini*/
+			if (IdxC < nbsamples) {
+				for (idxTime = IdxC; idxTime<nbsamples; idxTime++) {
+					rayonCut[idxTime-IdxC] = rmoy[idxTime];
+				}
+				rayonInfini[idxK] = gsl_stats_mean(rayonCut, 1, nbsamples-IdxC+1);
+//				printf("%g pour K = %d \n",rayonInfini[idxK], idxK);
+			}
+			else {
+//				printf("%g pour K= %d \n", rayonInfini[idxK], idxK);
+				rayonInfini[idxK] = gsl_stats_mean(rmoy, 1, nbsamples);
+/*car sinon on obtient des valeurs nulles du rayonInfini pour certaines valeurs de K<Kc*/
+			}
+
+
+			if (nbK == 1) {
+				rayonmoyen[0] = rayon[0];
+				for (idxTime = 1 ; idxTime < nbsamples ; idxTime++) {
+					rayonmoyen[idxTime] = rayonmoyen[idxTime-1] + rayon[idxTime];
+				}
+				for (idxTime = 0 ; idxTime < nbsamples ; idxTime++) {
+					rayonmoyen[idxTime] /= idxTime+1;
+				}
+			}
+	
+
+
 		}
 
 
+		/*Détermination des logarithmes*/
+		while (Kvect[idxKc] <= Kc) {
+			idxKc += 1;
+		}
+		idxKc += 4;
 
+		for (idxK = idxKc ; idxK < nbK ; idxK++) {
+			idxprim = idxK - idxKc;
+			logk[idxprim] = log(1 - Kc / Kvect[idxK]);
+			logr[idxprim] = log(rayonstable[idxK]);
+			deltarstable[idxK] = sqrt(1 - Kc / Kvect[idxK]) - rayonstable[idxK];
+			if (deltarstable[idxK] > ecartMax[idxN])
+				ecartMax[idxN] = deltarstable[idxK];
+			KvectCut[idxprim] = Kvect[idxK];
+			TcCut[idxprim] = Tc[idxK];
+		}
+		
+
+
+		/*Détermination de la distribution des pulsations propres*/
+		for (idxw=0 ; idxw < nbw ; idxw++) {
+			w[idxw] = (-(nbw - 1) / 2 + idxw) * 0.8 * 2 / nbw;
+//			N[idxw] = gsl_ran_cauchy_pdf(w[idxw], sigma);
+			N[idxw] = gsl_ran_cauchy_pdf(w[idxw] - 0.2, sigma) / 2 + gsl_ran_cauchy_pdf(w[idxw] + 0.2, sigma) / 2;
+		}
+	
 	}
 
-	/*Détermination des logarithmes*/
-	int idxKc = 0;
-	int idxprim = 0;
-	while (Kvect[idxKc] <= Kc) {
-		idxKc += 1;
-	}
-	idxKc += 4;
-	double *logk = (double *) malloc ((nbK-idxKc)*sizeof(double));
-	double *logr = (double *) malloc ((nbK-idxKc)*sizeof(double));
-	double *KvectCut = (double *) malloc ((nbK-idxKc)*sizeof(double));
-	double *TcCut = (double *) malloc ((nbK-idxKc)*sizeof(double));
-	for (idxK = idxKc ; idxK < nbK ; idxK++) {
-		idxprim = idxK - idxKc;
-		logk[idxprim] = log(1 - Kc / Kvect[idxK]);
-		logr[idxprim] = log(rayonstable[idxK]);
-		KvectCut[idxprim] = Kvect[idxK];
-		TcCut[idxprim] = Tc[idxK];
-	}
+	/*Détermination du vecteur "numéro de l'oscillateur"*/
+	double *Nosc = (double *) malloc (nbosc*sizeof(double));
 
-
-	/*Détermination de la distribution des pulsations propres*/
-	int nbw = 1001;
-	int idxw;
-	double *w = (double *) malloc (nbw*sizeof(double));
-	double *N = (double *) malloc (nbw*sizeof(double));
-	for (idxw=0 ; idxw < nbw ; idxw++) {
-		w[idxw] = (-(nbw - 1) / 2 + idxw) * 0.8 * 2 / nbw;
-		N[idxw] = gsl_ran_cauchy_pdf(w[idxw], sigma);
+	for(idxOsc = 0 ; idxOsc < nbosc ; idxOsc++) {
+		Nosc[idxOsc]=idxOsc;
 	}
 
 
@@ -277,6 +304,17 @@ int main(int argc, char *argv[])
 	gnuplot_cmd(gp, "set terminal wxt 1 persist");
 #endif
 	gnuplot_setstyle(gp, "linespoints");
+	gnuplot_set_ylabel(gp, "maximum de l'écart entre simulation et théorie");
+	gnuplot_set_xlabel(gp, "N");
+	gnuplot_plot_xy(gp, Nosc, ecartMax, nbosc, "evolution des fluctuations en fonction du nombre d'oscillateurs") ;
+
+	gp = gnuplot_init();
+#if defined ( __APPLE__ )
+	gnuplot_cmd(gp, "set terminal x11 2 persist");
+#else
+	gnuplot_cmd(gp, "set terminal wxt 2 persist");
+#endif
+	gnuplot_setstyle(gp, "linespoints");
 	gnuplot_set_ylabel(gp, "r");
 
 	if (nbK == 1) {
@@ -289,6 +327,17 @@ int main(int argc, char *argv[])
 		gnuplot_plot_xy(gp, temps, rayonmoyen, nbsamples, titre) ;
 		sprintf(titre,"evolution de rmoy(t) pour K = %f", K);
 		gnuplot_plot_xy(gp, temps, rmoy, nbsamples, titre);
+		
+		gp = gnuplot_init();
+#if defined ( __APPLE__ )
+		gnuplot_cmd(gp, "set terminal x11 3 persist");
+#else
+		gnuplot_cmd(gp, "set terminal wxt 3 persist");
+#endif
+		gnuplot_setstyle(gp, "lines");
+		gnuplot_set_ylabel(gp, "theta");
+		gnuplot_set_xlabel(gp, "numéro de l'oscillateur");
+		gnuplot_plot_xy(gp, Nosc, omega, nbosc,"angles des oscillateurs");
 	}
 
 	else {
@@ -299,9 +348,9 @@ int main(int argc, char *argv[])
 
 		gp = gnuplot_init();
 #if defined ( __APPLE__ )
-		gnuplot_cmd(gp, "set terminal x11 2 persist");
+		gnuplot_cmd(gp, "set terminal x11 3 persist");
 #else
-		gnuplot_cmd(gp, "set terminal wxt 2 persist");
+		gnuplot_cmd(gp, "set terminal wxt 3 persist");
 #endif
 		gnuplot_setstyle(gp, "linespoints");
 		gnuplot_set_ylabel(gp, "log rstable");
@@ -316,17 +365,31 @@ int main(int argc, char *argv[])
 		gnuplot_set_xlabel(gp, "K");
 
 #if defined ( __APPLE__ )
-    	gnuplot_cmd(gp, "set terminal x11 3 persist");
+    	gnuplot_cmd(gp, "set terminal x11 4 persist");
 #else
-		gnuplot_cmd(gp, "set terminal wxt 3 persist");
+		gnuplot_cmd(gp, "set terminal wxt 4 persist");
 #endif
 		gnuplot_setstyle(gp, "linespoints");
 		gnuplot_plot_xy(gp, KvectCut, TcCut, (nbK-idxKc), "evolution du temps caracteristique");
 	/*il y a un souci au niveau de l'avant dernier point pour nbK=20*/
 
+	/*Tracé de l'évolution de l'écart des r simulés et théoriques*/
+		gp = gnuplot_init();
+		gnuplot_set_ylabel(gp, "r théorique - r simulé");
+		gnuplot_set_xlabel(gp, "K");
+
+#if defined ( __APPLE__ )
+    	gnuplot_cmd(gp, "set terminal x11 5 persist");
+#else
+		gnuplot_cmd(gp, "set terminal wxt 5 persist");
+#endif
+		gnuplot_setstyle(gp, "linespoints");
+		gnuplot_plot_xy(gp, KvectCut, deltarstable, (nbK-idxKc), "écart entre simulation et théorie");
+
 	}
 
 	printf("r = %f\npsi = %f\n",rayontemp,psitemp);
+
 
 
 	/*Libération de la mémoire*/
